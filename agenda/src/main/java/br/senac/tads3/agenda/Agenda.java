c29017fc936c056c3bb4231421da8c06d0975371
pedro.blasembauer <pedro.blasembauer@CAS555070W0702.senacsp.edu.br>
2016-08-19 20:24:42 -0300

    Primeiro commit

---
+++ agenda/src/main/java/br/senac/tads3/agenda/Agenda.java
@@ -0,0 +1,8 @@
+package br.senac.tads3.agenda;
+
+public class Agenda {
+    //Funcao de gereciamento - MAIN
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo");
+    }
+}
